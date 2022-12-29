d587278d8a025a88ea3b0cb1e17e4b58345b422e
Vladimir Khlebnikov <107213679+khlebnikoff@users.noreply.github.com>
2022-12-29 17:02:08 +0300

    Delete Schreader.cpp

--- Schreader.cpp
+++
@@ -1,9 +0,0 @@
-#include "schreader.h"
-Schreader::Schreader(float powerKWt, float squareM2) :_powerKWt(powerKWt), _squareM2(squareM2)
-{
-
-}
-void Schreader::Show()
-{
-	cout << "Шредер Мощность КВТ: " << _powerKWt << " Занимаемая площадь М2: " << _squareM2 << endl;
-}
